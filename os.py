89ede3aff9775a2a30ace00d0ef29ae87f9c104e
Subhajit Das <73419022+Subha2001@users.noreply.github.com>
2025-03-15 16:21:45 +0530

    Update os.py
    Updated api key

--- os.py
+++ os.py
@@ -1,5 +1,5 @@
 # This file is used to store the environment variables
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "your_openai_api_key"
 PINECONE_API_KEY = "your_pinecone_api_key"
 PINECONE_ENVIRONMENT = "your_pinecone_environment"
 PINECONE_INDEX_NAME = "your_pinecone_index_name"
